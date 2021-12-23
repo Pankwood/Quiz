2445d3917eeddeccdb2d8f7fd397007f05e1ec9b
dan <dan.debiazi@gmail.com>
2021-12-23 13:10:22 -0500

    Added Results calculation

--- src/app/quiz-page/quiz-page.component.ts
+++ src/app/quiz-page/quiz-page.component.ts
@@ -12,23 +12,37 @@ export class QuizPageComponent implements OnInit {
 
   questionsList: any;
   clickStatus: Array<boolean>;
+  countCorrectAnswers: number;
   funnyMessage: string[];
   funnyIndexMessage: string;
+  questionsAnswered: number[];
+  quizContainer: any;
+  resultsContainer: any;
+
+
 
   constructor(quizService: QuizService) {
 
     this.questionsList = quizService.getQuiz();
     this.clickStatus = new Array(this.questionsList[0].answers.lenght);
+    this.countCorrectAnswers = 0;
     this.funnyMessage = ["Are you sure?", "Humm...Maybe...", "Seriously? HAHA"];
     this.funnyIndexMessage = "";
+    this.questionsAnswered = [];
+
   }
 
   ngOnInit(): void {
   }
 
   onClickMe($event: any, value: string, answerIndex: number, questionsListIndex: number) {
-    if ($event.target.value === this.questionsList[questionsListIndex].correctAnswer) {
+    if (($event.target.value === this.questionsList[questionsListIndex].correctAnswer) && (!this.questionsAnswered.find(answer => answer === questionsListIndex))) {
+      this.countCorrectAnswers++;
+      this.questionsAnswered.push(questionsListIndex);
       console.log(this.questionsList[questionsListIndex].correctAnswer, "Correct answer!");
+    }
+    else if ((this.countCorrectAnswers > 0) && ($event.target.value !== this.questionsList[questionsListIndex].correctAnswer)) {
+      this.countCorrectAnswers--;
     }
 
     this.funnyIndexMessage = this.funnyMessage[Math.floor(Math.random() * this.funnyMessage.length)];
@@ -36,6 +50,44 @@ export class QuizPageComponent implements OnInit {
     /*for (let j = 0; j <= this.questionsList[questionsListIndex].answers.length - 1; j++) {
       this.clickStatus[j] = (j === answerIndex);
     }*/
+  }
+
+  submitClick() {
+    console.log("Correct answers", this.countCorrectAnswers);
+    this.showResults();
+  }
+
+  showResults() {
+
+    this.quizContainer = document.getElementById('quiz');
+    this.resultsContainer = document.getElementById('results');
+
+    // gather answer containers from our quiz
+    const answerContainers = this.quizContainer.querySelectorAll('.answers');
+
+    // keep track of user's answers
+    let numCorrect = 0;
+
+    // for each question...
+    this.questionsList.forEach((currentQuestion: { correctAnswer: any; }, questionNumber: string | number) => {
+
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+      }
+
+    });
+
+    // show number of correct answers out of total
+    this.resultsContainer.innerHTML = numCorrect + ` out of ` + this.questionsList.length;
+    //window.location.href = "./result.html";
+    localStorage.setItem('numCorrect', numCorrect.toString());
   }
 
 }
