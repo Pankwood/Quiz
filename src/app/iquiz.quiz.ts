b7f891fdb8a9277715a6add0ce906a25a95921d4
dan <dan.debiazi@gmail.com>
2022-02-04 11:22:02 -0500

    Refactoring interface name

--- src/app/iquiz.quiz.ts
+++ src/app/iquiz.quiz.ts
@@ -1,16 +1,16 @@
 export interface IQuiz {
-    quizTitle: string;
+    title: string;
     questions: Array<IQuestion>;
 }
 
 export interface IQuestion {
     id: number;
-    quizQuestion: string;
+    question: string;
     answers: Array<IAnswer>;
 }
 
 
 export interface IAnswer {
-    quizAnswer: string;
+    answer: string;
     quizCorrectAnswer: boolean;
 }
