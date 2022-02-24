ea52ecca4f1b575a5ba5c79a639850e853853aa1
dan <dan.debiazi@gmail.com>
2022-02-24 01:01:38 -0500

    Auto-generated commit [ci skip]

--- src/app/check-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CheckValidatorDirective } from './check-validator.directive';
-
-describe('CheckValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CheckValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/duplicate-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DuplicateValidatorDirective } from './duplicate-validator.directive';
-
-describe('DuplicateValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DuplicateValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
